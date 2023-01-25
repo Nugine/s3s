0ebcd98dca817221188c92fbb61fc267ba967c36
Nugine <nugine@foxmail.com>
2023-01-25 22:40:11 +0800

    scripts: s3s-proxy

--- scripts/s3s-proxy.sh
+++ scripts/s3s-proxy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -ex
 
+mkdir -p /tmp/minio
 docker run -p 9000:9000 -p 9001:9001 \
     -e "MINIO_DOMAIN=localhost:9000" \
     -e "MINIO_HTTP_TRACE=1" \
+    -v /tmp/minio:/data \
     minio/minio:latest server /data --console-address ":9001" &
 
 sleep 1s
