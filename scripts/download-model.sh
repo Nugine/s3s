b9b055ce3199a1fad74766440177e50544c66487
Nugine <nugine@foxmail.com>
2023-02-02 12:19:33 +0800

    codegen: update model

--- scripts/download-model.sh
+++ scripts/download-model.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 mkdir -p target
 F="codegen/s3.json"
-COMMIT=2e49e06ecb041afaf3fbed5a4a4f6c6afbb052fa
+COMMIT=39d457149ac9ed01e73bb274dfe4341e82c8bbc3
 URL=https://github.com/awslabs/aws-sdk-rust/raw/$COMMIT/aws-models/s3.json
 wget --output-document $F $URL
