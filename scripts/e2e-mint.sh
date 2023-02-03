8395e1ba222e7d0d662c6014f3de28121cb1f914
Nugine <nugine@foxmail.com>
2023-02-03 14:26:49 +0800

    ci: mint

--- scripts/e2e-mint.sh
+++ scripts/e2e-mint.sh
@@ -1,5 +1,4 @@
-#!/bin/bash -ex
-./scripts/s3s-proxy.sh &
-sleep 5s
-./scripts/mint.sh > /tmp/mint.log
-cat /tmp/mint.log
+#!/bin/bash
+./scripts/s3s-proxy.sh > target/s3s-proxy.ansi &
+sleep 3s
+./scripts/mint.sh > target/mint.log
