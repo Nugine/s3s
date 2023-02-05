08eef72cd93e0807dc5c86742946c66d51e4a5d8
Nugine <nugine@foxmail.com>
2023-02-05 13:31:01 +0800

    ci: debug

--- scripts/e2e-mint.sh
+++ scripts/e2e-mint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-./scripts/s3s-proxy.sh > target/s3s-proxy.ansi &
+./scripts/s3s-proxy.sh | tee target/s3s-proxy.ansi &
 sleep 3s
-./scripts/mint.sh > target/mint.log
+./scripts/mint.sh | tee target/mint.log
