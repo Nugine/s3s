d81436802d0192fc645220b026fcfc4aad2ce411
Nugine <nugine@foxmail.com>
2023-03-31 20:24:31 +0800

    upgrade s3 model

--- scripts/download-model.sh
+++ scripts/download-model.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
+
+# https://github.com/awslabs/aws-sdk-rust/commits/main/aws-models/s3.json
+COMMIT=d85c1e7393b03e31fdc569bc07159dfc27812b9e
+
 mkdir -p target
 F="codegen/s3.json"
-COMMIT=39d457149ac9ed01e73bb274dfe4341e82c8bbc3
 URL=https://github.com/awslabs/aws-sdk-rust/raw/$COMMIT/aws-models/s3.json
 wget --output-document $F $URL
