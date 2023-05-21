ffd67882da1a0160b99a8e80bfe58985a3ea5df6
Nugine <nugine@foxmail.com>
2023-05-22 01:55:25 +0800

    deps: upgrade nugine-rust-utils

--- crates/s3s/src/ops/mod.rs
+++ crates/s3s/src/ops/mod.rs
@@ -681,36 +681,8 @@ mod tests {
     use super::*;
 
     use crate::service::S3Service;
-
-    pub trait OutputSize<A> {
-        type Output;
-    }
-
-    macro_rules! impl_output_size {
-        (($($ty:tt,)*)) => {
-            impl<$($ty,)* F, R> OutputSize<($($ty,)*)> for F
-            where
-                F: Fn($($ty,)*) -> R ,
-            {
-                type Output = R;
-            }
-        };
-    }
-
-    impl_output_size!(());
-    impl_output_size!((A0,));
-    impl_output_size!((A0, A1,));
-    impl_output_size!((A0, A1, A2,));
-    impl_output_size!((A0, A1, A2, A3,));
-
-    #[inline]
-    #[must_use]
-    pub const fn output_size<F, A>(_: &F) -> usize
-    where
-        F: OutputSize<A>,
-    {
-        std::mem::size_of::<F::Output>()
-    }
+    
+    use rust_utils::mem::output_size;
 
     #[test]
     #[ignore]
