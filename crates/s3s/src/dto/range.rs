fde7f6d811ac719887cb1426079ed3c8c100f7df
Nugine <nugine@foxmail.com>
2024-01-22 11:36:12 +0800

    fix satisfaction check

--- crates/s3s/src/dto/range.rs
+++ crates/s3s/src/dto/range.rs
@@ -103,32 +103,37 @@ impl Range {
     }
 
     /// Checks if the range is satisfiable
+    ///  according to [RFC9110](https://www.rfc-editor.org/rfc/rfc9110.html#name-byte-ranges).
     /// # Errors
     /// Returns an error if the range is not satisfiable
     #[allow(clippy::range_plus_one)] // cannot be fixed
     pub fn check(&self, full_length: u64) -> Result<ops::Range<u64>, RangeNotSatisfiable> {
         let err = || RangeNotSatisfiable { _priv: () };
         match *self {
-            Range::Int { first, last } => match last {
-                Some(last) => {
-                    let last = last.min(full_length - 1);
-                    if first > last {
-                        return Err(err());
-                    }
-                    // first <= last < full_length
-                    Ok(first..last + 1)
-                }
-                None => {
-                    if first > full_length {
-                        return Err(err());
-                    }
-                    Ok(first..full_length)
-                }
-            },
-            Range::Suffix { length } => {
-                if length > full_length {
+            Range::Int { first, last } => {
+                if first >= full_length {
                     return Err(err());
                 }
+                // 0 <= first < full_length
+
+                match last {
+                    Some(last) => {
+                        let last = last.min(full_length - 1);
+                        if first > last {
+                            return Err(err());
+                        }
+                        // 0 <= first <= last < full_length
+                        Ok(first..last + 1)
+                    }
+                    // 0 <= first < full_length
+                    None => Ok(first..full_length),
+                }
+            }
+            Range::Suffix { length } => {
+                if length == 0 {
+                    return Err(err());
+                }
+                let length = length.min(full_length);
                 Ok((full_length - length)..full_length)
             }
         }
@@ -194,6 +199,8 @@ mod tests {
             ("bytes=-500 ", Err(())),
             ("bytes=-+500", Err(())),
             ("bytes=-1000000000000000000000000", Err(())),
+            ("bytes=-0", Ok(range_suffix(0))),
+            ("bytes=-000", Ok(range_suffix(0))),
         ];
 
         for (input, expected) in &cases {
@@ -201,6 +208,32 @@ mod tests {
             match expected {
                 Ok(expected) => assert_eq!(output.unwrap(), *expected),
                 Err(()) => assert!(output.is_err()),
+            }
+        }
+    }
+
+    #[test]
+    fn satisfiable() {
+        let cases = [
+            (10000, range_int_from(9500), Ok(9500..10000)),
+            (10000, range_int_from(10000), Err(())),
+            (10000, range_int_inclusive(0, 499), Ok(0..500)),
+            (10000, range_int_inclusive(0, 0), Ok(0..1)),
+            (10000, range_int_inclusive(9500, 50000), Ok(9500..10000)),
+            (10000, range_int_inclusive(10000, 10000), Err(())),
+            (10000, range_suffix(500), Ok(9500..10000)),
+            (10000, range_suffix(10000), Ok(0..10000)),
+            (10000, range_suffix(0), Err(())),
+            (0, range_int_from(0), Err(())),
+            (0, range_suffix(1), Ok(0..0)),
+            (0, range_suffix(0), Err(())),
+        ];
+
+        for &(full_length, ref range, ref expected) in &cases {
+            let output = range.check(full_length);
+            match expected {
+                Ok(expected) => assert_eq!(output.unwrap(), *expected),
+                Err(()) => assert!(output.is_err(), "{full_length:?}, {range:?}"),
             }
         }
     }
