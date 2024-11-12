9d775eb119ff938b26372fd7a1867a5803e47082
Nugine <nugine@foxmail.com>
2024-11-12 11:27:30 +0800

    feat(model): crawl SelectObjectContent error codes

--- model/main.py
+++ model/main.py
@@ -1,15 +1,27 @@
 from pathlib import Path
+from pprint import pprint  # noqa: F401
+import re
+
+from bs4 import BeautifulSoup
 import requests
 import typer
+import json
 
 cli = typer.Typer()
 
 model_dir = Path(__file__).parent
 
 
+def save_json(path, data):
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
+
+
 def download_aws_sdk(service: str, *, commit: str):
     url = f"https://github.com/awslabs/aws-sdk-rust/raw/{commit}/aws-models/{service}.json"
     resp = requests.get(url)
+    assert resp.status_code == 200
+    assert resp.json()
     with open(model_dir / f"{service}.json", "w") as f:
         f.write(resp.text)
 
@@ -27,9 +39,64 @@ def download_sts_model():
 
 
 @cli.command()
+def crawl_error_codes():
+    url = "https://docs.aws.amazon.com/AmazonS3/latest/API/ErrorResponses.html"
+
+    html = requests.get(url).text
+
+    soup = BeautifulSoup(html, "lxml")
+
+    h2_id = "SelectObjectContentErrorCodeList"
+
+    h2 = soup.css.select(f"#{h2_id}")[0]  # type:ignore
+
+    # find the next table
+    table = None
+    for e in h2.next_elements:
+        if e.name == "table":  # type:ignore
+            table = e
+            break
+    assert table is not None
+
+    th_list = table.css.select("th")  # type:ignore
+    assert th_list[0].text == "Error code"
+    assert th_list[1].text == "Description"
+    assert th_list[2].text == "HTTP status code"
+
+    tr_list = table.css.select("tr")[1:]  # type:ignore
+    tr_list = [[e for e in tr.children if e.name == "td"] for tr in tr_list]
+
+    ans = []
+    for td_list in tr_list:
+        t0 = td_list[0].css.select("code")[0].text
+        t1 = td_list[1].text
+        t2 = td_list[2].text
+
+        error_code = t0
+
+        description = re.sub(r"\n\t+", " ", t1).strip()
+
+        if t2 == "405 Method Not Allowed":
+            http_status_code = 405
+        else:
+            http_status_code = int(t2)
+
+        ans.append(
+            {
+                "error_code": error_code,
+                "description": description,
+                "http_status_code": http_status_code,
+            }
+        )
+
+    save_json(model_dir / "s3_error_codes.json", {"SelectObjectContent": ans})
+
+
+@cli.command()
 def update():
     download_s3_model()
     download_sts_model()
+    crawl_error_codes()
 
 
 if __name__ == "__main__":
