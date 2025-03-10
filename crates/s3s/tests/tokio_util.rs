cfebeb13ffcd21cb4421328b8a99fa2ca3d66433
Nugine <nugine@foxmail.com>
2025-03-10 15:32:44 +0800

    test(s3s): tokio_util StreamReader

---
+++ crates/s3s/tests/tokio_util.rs
@@ -0,0 +1,11 @@
+use futures::TryStreamExt;
+use tokio::io::AsyncBufRead;
+use tokio_util::io::StreamReader;
+
+pub fn convert_body(body: s3s::Body) -> impl AsyncBufRead + Send + Sync + 'static {
+    StreamReader::new(body.into_stream().map_err(std::io::Error::other))
+}
+
+pub fn convert_streaming_blob(blob: s3s::dto::StreamingBlob) -> impl AsyncBufRead + Send + Sync + 'static {
+    StreamReader::new(blob.into_stream().map_err(std::io::Error::other))
+}
