96fa0a2f11d0ce3ad81f78c13c0a9aa89fe08218
Nugine <nugine@foxmail.com>
2025-03-12 21:38:04 +0800

    refactor(s3s): move examples

--- crates/s3s/examples/tokio_util.rs
+++ crates/s3s/examples/tokio_util.rs
@@ -9,3 +9,5 @@ pub fn convert_body(body: s3s::Body) -> impl AsyncBufRead + Send + Sync + 'stati
 pub fn convert_streaming_blob(blob: s3s::dto::StreamingBlob) -> impl AsyncBufRead + Send + Sync + 'static {
     StreamReader::new(blob.into_stream().map_err(std::io::Error::other))
 }
+
+fn main() {}
